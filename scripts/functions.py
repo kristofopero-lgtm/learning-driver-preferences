09e0dd8a8015601d0b45761d5d30f7156804cf22
Kristof <kristof.opero@gmail.com>
2026-01-20 20:28:39 +0100

    Updated the functions.py file to include a function to create a DF of all responses; and join the requests and responses DFs to add a sequence of tasks / locations to the combined DF.

--- scripts/functions.py
+++ scripts/functions.py
@@ -16,14 +16,16 @@ def create_df_of_requests(path_to_parent_folder):
                 route_id = file.split('-')[0].split('_')[1]
                 date = pd.to_datetime(file.split('-')[1], format="%Y%m%d")
                 time = pd.to_datetime(file.split('-')[2], format="%H%M%S").time()
-                with open(os.path.join(path, file), "r") as f:
+                with open(os.path.join(path, file), "r") as f:                    
                     data = json.load(f)
                     request_type = data["configurationName"]
                     request_id = data["id"]
-                    tasks = data["tasks"]
+                    tasks = data["tasks"]                    
                     fixed_tasks = [task["taskId"] for task in data["fixedTasks"]]
                     for task in tasks:
                         row = {}
+                        row_id = file.split('.')[-2] + '-' + task["id"]
+                        row["row_id"] = row_id
                         row["route_id"] = route_id
                         row["date"] = date
                         row["time"] = time
@@ -40,3 +42,40 @@ def create_df_of_requests(path_to_parent_folder):
                         rows.append(row)
 
     return pd.DataFrame(rows)
+
+
+def create_df_of_responses(path_to_parent_folder):
+    dirs = os.listdir(path_to_parent_folder)
+
+    rows = []
+
+    for dir in dirs:
+        path = os.path.join(path_to_parent_folder, dir)
+        if os.path.isdir(path):
+            files = os.listdir(path)
+            for file in files:
+                route_id = file.split('-')[0].split('_')[1]
+                date = pd.to_datetime(file.split('-')[1], format="%Y%m%d")
+                time = pd.to_datetime(file.split('-')[2], format="%H%M%S").time()
+                with open(os.path.join(path, file), "r") as f:
+                       tasks = f.readlines()
+                       for task in tasks:
+                        row = {}
+                        row_id = file.split('.')[-2] + '-' + task.strip()
+                        row["row_id"] = row_id
+                        row['route_id'] = route_id
+                        row['date'] = date
+                        row['time'] = time                        
+                        row['task_id'] = task.strip()
+                        row['task_sequence_number'] = tasks.index(task) + 1
+                        rows.append(row)
+
+    return pd.DataFrame(rows)
+
+
+def join_requests_and_responses(requests_df, responses_df):
+    return requests_df.merge(
+        responses_df[["row_id", "task_sequence_number"]],
+        on='row_id',
+        how="inner"
+    )
